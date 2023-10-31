ab2067189fdfee8628e812ff624422b4bf87b8b7
Cem Arcan <cmrcn01@gmail.com>
2023-10-31 19:12:55 +0300

    referanslar editleri

--- src/pages/Referanslar.jsx
+++ src/pages/Referanslar.jsx
@@ -1,7 +1,103 @@
-import React from 'react'
+import React from "react";
 
 export default function Referanslar() {
   return (
-    <div>Referanslar</div>
-  )
+    <div className="flex max-w-6xl mx-auto px-6">
+      <div className="max-w-6xl">
+        <h2 className="text-4xl text-gray-950 my-6">REFERANSLARIMIZ</h2>
+        <div className=" flex flex-wrap mt-6 justify-around space-x-4">
+          <div>
+            <img src="src\assets\images\Müşteriler\arcelik_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              ARÇELİK A.Ş.
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\bosch_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              BOSCH A.Ş.
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\festo_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              FESTO
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\hktm_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              HKTM
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\farba_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              FARBA A.Ş.
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\mako_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              MAKO A.Ş.
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\aircraft_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              AIRCRAFT
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\sakin-makina_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              SAKİN MAKİNA
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\arikanotomotiv_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              ARIKAN OTOMOTİV
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\marmara-universitesi_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              MARMARA ÜNİ.
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\tanatar-kalip_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              TANATAR KALIP
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\onder-haliyikama_1_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              ÖNDER HALI YIKAMA
+            </p>
+          </div>
+
+          <div>
+            <img src="src\assets\images\Müşteriler\inka-makina_large.jpg" alt="" />
+            <p className=" block text-center font-bold text-gray-950 text-xl mt-4 mb-12">
+              İNKA MAKİNA
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }

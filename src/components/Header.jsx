94d6c33d0e31ebc3461607d32b7b6822946a7e81
Cem Arcan <cmrcn01@gmail.com>
2023-10-31 16:58:39 +0300

    header and carousel changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,45 +7,67 @@ export default function Header() {
       <div className=" flex justify-between items-center max-w-6xl mx-auto p-3 h-32">
         <Link to="/">
           <img
-            className=" w-80"
+            className=" flex w-auto"
             src="src\assets\images\libramech-WBG.png"
             alt=""
           />
         </Link>
 
-        <ul className=" flex gap-7">
+        <ul className="flex gap-7">
           <Link to="/">
-            <li className=" text-black underline underline-offset-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
+            <li className=" text-black transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
               Anasayfa
             </li>
           </Link>
 
           <Link to="/hakkımızda">
-            <li className=" text-black underline underline-offset-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
+            <li className=" text-black transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
               Hakkımızda
             </li>
           </Link>
 
           <Link to="/projeler">
-            <li className=" text-black underline underline-offset-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
+            <li className=" text-black transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
               Projeler
             </li>
           </Link>
 
           <Link to="/hizmetler">
-            <li className=" text-black underline underline-offset-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
-              Hizmetler
-            </li>
+            <div className="dropdown dropdown-hover">
+              <button tabindex="0" className="cursor-pointer text-black transition ease-in-out delay-50 hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
+                Hizmetler
+              </button>
+              <ul
+                tabindex="0"
+                className="dropdown-content z-[1] menu p-4 shadow bg-base-100 rounded-box w-40"
+              >
+                <li className="bg-orange-400 rounded-md">
+                  <a>Danışmanlık</a>
+                </li>
+                <li>
+                  <a>İmalat</a>
+                </li>
+                <li>
+                  <a>Mekatronik</a>
+                </li>
+                <li>
+                  <a>Proje</a>
+                </li>
+                <li>
+                  <a>Temsilcilik</a>
+                </li>
+              </ul>
+            </div>
           </Link>
 
           <Link to="/referanslar">
-            <li className=" text-black underline underline-offset-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
+            <li className=" text-black transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
               Referanslar
             </li>
           </Link>
 
           <Link to="/iletişim ">
-            <li className=" text-black underline underline-offset-4 transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
+            <li className=" text-black transition ease-in-out delay-50  hover:-translate-y-1 hover:scale-110  duration-300 font-medium">
               İletişim
             </li>
           </Link>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,14 @@ import React from "react";
 export default function Home() {
   return (
     <div>
-      <div className="carousel w-full h-96 ">
+      <div className="carousel w-auto h-96 ">
         <div id="slide1" className="carousel-item relative w-full">
-          <img src="src\assets\images\Anasayfa\001.jpg" className="w-full" />
+          <img src="src\assets\images\Anasayfa\001.jpg" className="w-full " />
+          <div className=" absolute flex max-w-2xl ml-44 mt-24 ">
+            <h1 className="text-5xl font-bold text-white">
+              Eğer doğru seçimi yapamazsak, onlar da hata yapar.
+            </h1>
+          </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -16,8 +21,13 @@ export default function Home() {
           </div>
         </div>
 
-        <div id="slide2" className="carousel-item relative w-full">
+        <div id="slide2" className="carousel-item relative w-full ">
           <img src="src\assets\images\Anasayfa\002.JPG" className="w-full" />
+          <div className=" absolute flex max-w-2xl ml-44 mt-24 ">
+            <h1 className="text-5xl font-bold text-white">
+              Eğer doğru seçimi yapamazsak, onlar da hata yapar.
+            </h1>
+          </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -30,6 +40,11 @@ export default function Home() {
 
         <div id="slide3" className="carousel-item relative w-full">
           <img src="src\assets\images\Anasayfa\003.jpg" className="w-full" />
+          <div className=" absolute flex max-w-2xl ml-44 mt-24 ">
+            <h1 className="text-5xl font-bold text-white">
+              Eğer doğru seçimi yapamazsak, onlar da hata yapar.
+            </h1>
+          </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -42,6 +57,11 @@ export default function Home() {
 
         <div id="slide4" className="carousel-item relative w-full">
           <img src="src\assets\images\Anasayfa\004.png" className="w-full" />
+          <div className=" absolute flex max-w-2xl ml-44 mt-24 ">
+            <h1 className="text-5xl font-bold text-white">
+              Eğer doğru seçimi yapamazsak, onlar da hata yapar.
+            </h1>
+          </div>
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide3" className="btn btn-circle">
               ❮
